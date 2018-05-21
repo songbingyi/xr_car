defff73e4e7ee1e88ba911f0e67da83308a03b1d
请叫我一米 <yimity@gmail.com>
2018-05-21 21:04:59 +0800

    修复完成提交订单页面，跳到订单详情出错

--- weChat/src/pages/success/success.ts
+++ weChat/src/pages/success/success.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {BaseProvider} from '../../providers/http/base.http';
 import {ActivatedRoute, Router} from '@angular/router';
 import {IdentityAuthService} from '../../providers/identityAuth.service';
+import {LocalStorage} from '../../providers/localStorage';
 
 @Component({
     selector    : 'app-success',
@@ -15,12 +16,12 @@ export class SuccessComponent implements OnInit {
     isLoaded: Boolean = false;
     orderId: Number;
 
-    constructor(private route : ActivatedRoute, private router : Router, private baseProvider : BaseProvider, private identityAuthService:IdentityAuthService) {
+    constructor(private localStorage: LocalStorage, private route : ActivatedRoute, private router : Router, private baseProvider : BaseProvider, private identityAuthService:IdentityAuthService) {
         this.identityAuthService.check();
     }
 
     ngOnInit() {
-
+        //let member_role_list = this.localStorage.getObject('member_role_list');
     }
 
 }
